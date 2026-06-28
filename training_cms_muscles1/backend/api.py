a6924fb74ab7048b1bd09b0b28b56d20849426cd
Artur7840 <141182447+Artur7840@users.noreply.github.com>
2026-06-28 21:35:05 +0300

    Update api.py

--- training_cms_muscles1/backend/api.py
+++ training_cms_muscles1/backend/api.py
@@ -72,11 +72,3 @@ def create_app():
         return send_from_directory('../frontend', path)
 
     return app
-
-
-# Глобальный объект приложения для Gunicorn
-app = create_app()
-
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=False)
